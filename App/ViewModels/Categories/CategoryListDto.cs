14536eeacee1819fa91f467afe321a8c7eea3d7d
Ali Torabi <AliTorabi2020@Gmail.com>
2023-11-28 09:27:21 +0100

    Update CategoryListDto.cs

--- App/ViewModels/Categories/CategoryListDto.cs
+++ App/ViewModels/Categories/CategoryListDto.cs
@@ -1,10 +1,21 @@
-﻿using App.ViewModels.Common;
+using App.ViewModels.Common;
+using Data.Utility;
 
 namespace App.ViewModels.Categories;
 
-public record CategoryListDto : ContentViewModel
+public record CategoryListDto : BaseViewModel
 {
     public bool IsPin { get; set; }
+
+    public string Title { get; set; }
+
+    public string Url { get; set; }
+
+    public string Text { get; set; }
+
+    public string Summary => StringUtility.Summary(Text);
+
+    public string ImageUrl { get; set; }
 
     public string ParentTitle { get; set; }
 
